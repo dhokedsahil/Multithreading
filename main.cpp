b6ea88b07d778b1ef85a4a9561af7888176ba2c7
dhokedsahil <dhokedsahil@yahoo.co.in>
2018-01-24 14:40:10 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include "Parallel Linked List/LinkedList.cpp"
+#include "Parallel Linked List/Nodeptr.cpp"
+#include "Parallel Linked List/Node.cpp"
+#include <iostream>
+
+void LinkedListInteractive()
+{
+	ConcurrentLinkedList ll;
+	bool loop = true;
+	int action = -1;
+	int key;
+	while(loop)
+	{
+		std::cout << "1. Insert\n2. Remove\n3. Search\n4. Display\n5. Exit\nChoose an action: ";
+		cin >> action;
+		switch(action)
+		{
+			case 1: std::cout << "Key: "; cin >> key; if(ll.insert(key).pointer == NULL) std::cout << "Insert failed\n"; else std::cout << "Insert succeeded\n"; break;
+			case 2: std::cout << "Key: "; cin >> key; if(ll.remove(key).pointer == NULL) std::cout << "Remove failed\n"; else std::cout << "Remove succeeded\n"; break;
+			case 3: std::cout << "Key: "; cin >> key; if(ll.search(key).pointer == NULL) std::cout << "Search failed\n"; else std::cout << "Search succeeded\n"; break;
+			case 4: ll.print(); std::cout << std::endl; break;
+			case 5: loop = false; break;
+			default: std::cout << "Invalid action\n\n"; break;
+		}
+	}
+}
+
+int main()
+{
+	LinkedListInteractive();
+	return 0;
+}
