050ccfed730f6d5ec843444dbec7b1ca015f2c19
Sahil <dhokedsahil@yahoo.co.in>
2018-03-26 15:16:26 -0500

    Hazard Pointers

--- HazardPointers/HazardPointers.cpp
+++ HazardPointers/HazardPointers.cpp
@@ -1,1 +1,63 @@
 #include "HazardPointers.h"
+#include <unordered_set>
+
+template <class T>
+HazardPointersContainer<T>::HazardPointersContainer(int threads, int pointersPerThread)
+{
+    this->threads = threads;
+    HazardPointers = std::vector<HPRecType<T>>(threads, HPRecType<T>(this, pointersPerThread));
+}
+
+template <class T>
+HPRecType<T>::HPRecType(HazardPointersContainer<T>* baseContainer, int pointersPerThread)
+{
+    this->pointersPerThread = pointersPerThread;
+    this->baseContainer = baseContainer;
+    this->pointers = std::vector<HP<T>>(pointersPerThread, HP<T>(this));
+}
+
+template <class T>
+void HPRecType<T>::scan()
+{
+    std::vector<T*> temp;
+    rlists.swap(temp);
+    std::unordered_set<T*> plist;
+    //Insert all HPs of all threads to plist
+    for(auto const &hpRecType : baseContainer->HazardPointers)
+    {
+        for(auto const &hp : hpRecType->pointers)
+        {
+            plist.insert(hp.pointer);
+        }
+    }
+
+    for(auto &nodeptr : temp)
+    {
+        if(plist.find(nodeptr) != plist.end())
+        {
+            rlists.push_back(nodeptr);
+        }
+        else
+        {
+            delete nodeptr;
+        }
+    }
+}
+
+template <class T>
+HP<T>::HP(HPRecType<T>* baseHPRecType)
+{
+    this->baseHPRecType = baseHPRecType;
+    pointer = NULL;
+}
+
+template <class T>
+void HP<T>::retireNode()
+{
+    baseHPRecType->rlists.push_back(pointer);
+    pointer = NULL;
+    if(baseHPRecType->rlists.size() > baseHPRecType->rlists->threshold)
+    {        
+        baseHPRecType->scan();
+    }
+}
--- HazardPointers/HazardPointers.h
+++ HazardPointers/HazardPointers.h
@@ -3,24 +3,28 @@
 
 #include <vector>
 
+template <class T> class HPRecType;
+template <class T> class HP;
+
 template <class T>
 class HazardPointersContainer
 {
 public:
     int threads;//n
-    int pointersPerThread;//K
+    std::vector<HPRecType<T>> HazardPointers;
     HazardPointersContainer(int threads, int pointersPerThread);
-    vector<HPRecType<T>> HazardPointers;
 };
 
 template <class T>
 class HPRecType
 {
 public:
+    int threshold = 50;
+    int pointersPerThread;//K
     HazardPointersContainer<T>* baseContainer;
-    vector<HP<T>> pointers;
-    vector<T*> rlists;
-    void retireNode(T*);
+    std::vector<HP<T>> pointers;
+    std::vector<T*> rlists;
+    HPRecType(HazardPointersContainer<T>* baseContainer, int pointersPerThread);
     void scan();
 };
 
@@ -28,7 +32,10 @@ template <class T>
 class HP
 {
 public:
+    HPRecType<T>* baseHPRecType;
     T* pointer;
+    HP(HPRecType<T>* baseHPRecType);
+    void retireNode();
 };
 
 #endif
